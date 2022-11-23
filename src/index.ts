1335666b31027cfeaf85b3ff9e09a38e8f0c89dc
Takahiro Mitsuoka <tmitsuoka0423@gmail.com>
2022-11-23 19:09:52 +0900

    upd

--- src/index.ts
+++ src/index.ts
@@ -1,98 +1,53 @@
-import VanillaSwipe, { EventData } from './lib';
-
-if (module.hot) module.hot.accept();
-
-const element = document.getElementById('pad');
-const target = document.getElementById('stick');
-const shadow = document.getElementById('shadow');
-const info = document.getElementById('info');
-const resetBtn = document.getElementById('reset');
-const state = { isSwipeStarted: false, delta: 15, directionDelta: 15 };
+import VanillaSwipe from "vanilla-swipe";
 
 const VS = new VanillaSwipe({
-  element,
-  target,
-  delta: state.delta,
-  directionDelta: state.directionDelta,
+  element: document.getElementById("touch-area"),
+  onSwiped: handler,
   mouseTrackingEnabled: true,
-  preventTrackingOnMouseleave: true,
-  preventDefaultTouchmoveEvent: true,
-  onSwipeStart,
-  onSwiping,
-  onSwiped,
-  onTap,
 });
-
 VS.init();
-reset();
 
-if (resetBtn) {
-  resetBtn.onclick = reset;
+const label = document.getElementById("timer-label") as HTMLInputElement;
+const time = document.getElementById("timer-time") as HTMLInputElement;
+const result = document.getElementById("result") as HTMLElement;
+
+function setLabelWithAutoReset({
+  element,
+  text,
+  time = 5000,
+}: {
+  element: HTMLElement;
+  text: string;
+  time?: number;
+}) {
+  element.innerText = text;
+  setTimeout(() => {
+    element.innerText = "";
+  }, time);
 }
 
-function onTap(e: Event, data: EventData) {
-  return handler(e, data, 'onTap');
-}
-
-function onSwipeStart() {
-  state.isSwipeStarted = true;
-}
-
-function onSwiping(e: Event, data: EventData) {
-  if (shadow) {
-    const { deltaX, deltaY } = data;
-    shadow.style.cssText = `
-      display: block;
-      transform: translate(${deltaX}px, ${-deltaY}px);
-    `;
+function handler() {
+  console.log(arguments[1].directionY);
+  if (arguments[1].directionY !== "TOP") {
+    return;
   }
-  return handler(e, data, 'onSwiping');
-}
 
-function onSwiped(e: Event, data: EventData) {
-  if (shadow) shadow.style.cssText = '';
-  state.isSwipeStarted = false;
-  return handler(e, data, 'onSwiped');
-}
+  console.log("swiped up");
+  console.log({ label: label.value, time: time.value });
 
-function handler(e: Event, data: EventData, listener: string ) {
-  const { deltaX, deltaY, absX, absY, directionX, directionY, duration, velocity } = data;
-
-  if (info) {
-    info.innerText = `
-      EventType: ${e.type}
-      Listener: ${listener}
-      DeltaX: ${deltaX}
-      DeltaY: ${deltaY}
-      AbsX: ${absX}
-      AbsY: ${absY}
-      DirectionX: ${directionX}
-      DirectionY: ${directionY}
-      Duration: ${duration}
-      Velocity: ${velocity}
-      Delta: ${state.delta}px
-      DirectionDelta: ${state.directionDelta}px
-      SwipeStarted: ${state.isSwipeStarted}
-    `;
+  if (!label.value || !time.value) {
+    setLabelWithAutoReset({
+      element: result,
+      text: "時間とラベルを両方入力してください。",
+    });
+    return;
   }
-}
 
-function reset() {
-  if (info) {
-    info.innerText = `
-        EventType: none
-        Listener: none
-        DeltaX: 0
-        DeltaY: 0
-        AbsX: 0
-        AbsY: 0
-        DirectionX: none
-        DirectionY: none
-        Duration: 0
-        Velocity: 0
-        Delta: ${state.delta}px
-        DirectionDelta: ${state.directionDelta}px
-        SwipeStarted: false
-    `;
-  }
+  localStorage.setItem("test", JSON.stringify({ label, time }));
+  setLabelWithAutoReset({
+    element: result,
+    text: `${time.value}にアラームを設定しました。`,
+  });
+  label.value = "";
+  time.value = "";
 }
